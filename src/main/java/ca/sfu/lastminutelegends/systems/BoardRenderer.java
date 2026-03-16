feb10a732f4abba2de1277b5092fac00a9f97efa
caelwarner <caelawarner@gmail.com>
2026-03-16 01:32:35 -0700

    Start adding textures
    - Add floor and wall texture
    - Add texture loading and drawing to BoardRenderer

--- src/main/java/ca/sfu/lastminutelegends/systems/BoardRenderer.java
+++ src/main/java/ca/sfu/lastminutelegends/systems/BoardRenderer.java
@@ -3,9 +3,24 @@ package ca.sfu.lastminutelegends.systems;
 import ca.sfu.lastminutelegends.Game;
 import ca.sfu.lastminutelegends.board.*;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class BoardRenderer implements GameSystem {
+    private BufferedImage wallTexture;
+    private BufferedImage floorTexture;
+    
+    public BoardRenderer() {
+        try {
+            wallTexture = ImageIO.read(getClass().getResourceAsStream("/textures/wall.png"));
+            floorTexture = ImageIO.read(getClass().getResourceAsStream("/textures/floor.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
     @Override
     public void tick(int tick) {
         
@@ -17,8 +32,17 @@ public class BoardRenderer implements GameSystem {
             for (int x = 0; x < Game.instance().getBoard().getWidth(); x++) {
                 Cell cell = Game.instance().getBoard().getCell(x, y);
 
+                if (cell instanceof Wall) {
+                    g.drawImage(wallTexture, 50 + x * 50, 50 + y * 50, 50, 50, null);
+                    continue;
+                }
+                
+                if (cell instanceof EmptyCell) {
+                    g.drawImage(floorTexture, 50 + x * 50, 50 + y * 50, 50, 50, null);
+                    continue;
+                }
+                
                 switch (cell) {
-                    case Wall _ -> g.setColor(Color.DARK_GRAY);
                     case StartPoint _ -> g.setColor(Color.BLUE);
                     case EndPoint _ -> g.setColor(Color.GREEN);
                     case null, default -> g.setColor(Color.LIGHT_GRAY);
